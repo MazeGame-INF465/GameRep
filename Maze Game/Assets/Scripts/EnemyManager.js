1a33fb12c5da5ec98662024beabe403558ac8205
Edaf S <edafsarwar@gmail.com>
2015-02-06 17:03:04 -0500

    [Frog] Spawn Point 1

---
+++ Maze Game/Assets/Scripts/EnemyManager.js
@@ -0,0 +1,26 @@
+﻿//var playerHealth : PlayerHealth;       // Reference to the player's heatlh.
+var enemy : GameObject;                // The enemy prefab to be spawned.
+var spawnTime : float = 10f;            // How long between each spawn.
+var spawnPoints : Transform[];         // An array of the spawn points this enemy can spawn from.
+
+
+function Start (){
+    
+    // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+    InvokeRepeating ("Spawn", spawnTime, spawnTime);
+}
+
+
+function Spawn (){
+
+    /*// If the player has no health left, exit
+    if(playerHealth.currentHealth <= 0f){
+        return;
+    }*/
+
+    // Find a random index between zero and one less than the number of spawn points.
+    var spawnPointIndex : int = Random.Range (0, spawnPoints.Length);
+
+    // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+    Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+}
