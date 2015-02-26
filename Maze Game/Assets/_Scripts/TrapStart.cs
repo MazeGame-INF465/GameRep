042eb2c30ec3d1774b6285ae5593d0a1ad5cf300
zachpappalau <z.pappalau@gmail.com>
2015-02-26 00:42:28 -0500

    Added first Trap For Maze
    tired....

---
+++ Maze Game/Assets/_Scripts/TrapStart.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapStart : MonoBehaviour 
+{
+
+	public float smoothing = 1f;
+	public GameObject TrapComponent1;
+	public GameObject TrapComponent2;
+	public GameObject TrapComponent3;
+	Vector3 MoveLeft = new Vector3 (92.04f, 0f, 12.65f);
+	Vector3 MoveRight = new Vector3 (90.44f, 0f, 14.04f);
+	Vector3 BlockExit = new Vector3 (94.97f, 2.38f, -11.77f);
+		
+	public void StartTrap(string TrapName)
+	{
+		if(TrapName == "WaterFallTrap")
+		{
+			//TrapComponent1 = this.gameObject.transform.GetChild(2).transform;
+			//TrapComponent2 = this.gameObject.transform.GetChild(3).transform;
+			//TrapComponent1.SetActive(true);
+			//TrapComponent2.SetActive(true);
+			StartCoroutine(VineTrapLeft());
+			StartCoroutine(VineTrapRight());
+			StartCoroutine(VineTrapExit());
+		}
+	}
+	
+
+	IEnumerator VineTrapExit()
+	{
+		while(Vector3.Distance(BlockExit,TrapComponent3.transform.position) > 0.05f)
+		{
+			TrapComponent3.transform.position = Vector3.Lerp(TrapComponent3.transform.position, BlockExit, smoothing * Time.deltaTime);
+			
+			yield return null;
+		}
+		
+		print("Vine Exit Finished");
+	}
+
+	IEnumerator VineTrapRight()
+	{
+		while(Vector3.Distance(MoveRight,TrapComponent1.transform.position) > 0.05f)
+		{
+			TrapComponent1.transform.position = Vector3.Lerp(TrapComponent1.transform.position, MoveRight, smoothing * Time.deltaTime);
+			
+			yield return null;
+		}
+		
+		print("Vine Right Finished");
+	}
+
+	IEnumerator VineTrapLeft()
+	{
+		while(Vector3.Distance(MoveLeft,TrapComponent2.transform.position) > 0.05f)
+		{
+			TrapComponent2.transform.position = Vector3.Lerp(TrapComponent2.transform.position, MoveLeft, smoothing * Time.deltaTime);
+			
+			yield return null;
+		}
+		
+		print("Vine Left Finished");
+	}
+
+}
---
+++ Maze Game/Assets/_Scripts/TriggerEvent.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TriggerEvent : MonoBehaviour {
+	GameObject Gate;
+	bool GateInRange;
+	bool TrapTrig = false;
+	bool ExitTrapTrig =false;
+	bool TrapEvent = false;
+	bool VineTrap = false;
+	GameObject ParentObject = null;
+	public Text ActionText;
+
+	private TrapStart trapstart;
+	// Use this for initialization
+	void Awake ()
+	{
+		Gate = GameObject.FindGameObjectWithTag ("GateCollider");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GateInRange) 
+		{
+			if(Input.GetKeyDown (KeyCode.E))
+			{
+				Application.LoadLevel("Tower_Level");
+			}
+		}
+		if(!TrapTrig && ExitTrapTrig)
+		{
+			//Event Possible
+			TrapEvent=true;
+		}
+		if(!ExitTrapTrig && TrapTrig)
+		{
+			//Event Not Possible 
+			TrapEvent=false;
+		}
+		if(TrapEvent && !TrapTrig && !ExitTrapTrig)
+		{
+			trapstart = ParentObject.GetComponent<TrapStart>();
+			//Start Trap
+			Debug.Log("TrapStart");
+			if(!VineTrap)
+			{
+				trapstart.StartTrap(ParentObject.name);
+				VineTrap= true;
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log("In Trigger");
+		if(other.gameObject == Gate)
+		{
+			GateInRange = true;
+			ActionText.text = "Press space (e) to Enter level";
+			Debug.Log("Found gate");
+		}
+		if(other.gameObject.tag == "Trap")
+		{
+			Debug.Log("InTrap");
+			TrapTrig=true;
+			ParentObject = other.gameObject.transform.parent.gameObject;
+		}
+		if(other.gameObject.tag == "ExitTrap")
+		{
+			Debug.Log("InTrapExit");
+			ExitTrapTrig=true;
+		}
+
+
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == Gate)
+		{
+			GateInRange = false;
+			ActionText.text = "";
+		}
+		if(other.gameObject.tag == "Trap")
+		{TrapTrig=false;}
+		if(other.gameObject.tag == "ExitTrap")
+		{ExitTrapTrig=false;}
+	}
+}
